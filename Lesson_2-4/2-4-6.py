9ce07faef88af89201810ae918773680649909b3
Entir <Blazhennovanton@yandex.ru>
2024-01-04 20:02:12 +0300

    lesson 2-4-(9-14)

---
+++ Lesson_2-4/2-4-6.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = a + 1
+c = b + 1
+print(a)
+print(b)
+print(c)
---
+++ Lesson_2-4/2-4-8.py
@@ -0,0 +1,5 @@
+a = int(input())
+vol = a*a*a
+s = 6*(a*a)
+print('Объем =', vol)
+print('Площадь полной поверхности =', s)
