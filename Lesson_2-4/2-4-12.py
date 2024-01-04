3e4d2823dfd238b086a6223fbe2ac7029f5968fa
Entir <Blazhennovanton@yandex.ru>
2024-01-04 20:00:49 +0300

    lesson 2-4-(9-14)

---
+++ Lesson_2-4/2-4-12.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+sum = a+b
+min = a-b
+mul = a*b
+print(a, '+', b, '=', sum)
+print(a, '-', b, '=', min)
+print(a, '*', b, '=', mul)
---
+++ Lesson_2-4/2-4-14.py
@@ -0,0 +1,6 @@
+x = int(input())
+x2 = x*2
+x3 = x*3
+x4 = x*4
+x5 = x*5
+print(x, x2, x3, x4, x5, sep='---')
